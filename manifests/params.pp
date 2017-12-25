24eb9a7d42274a6c839070fc7fbe7a304790167a
Randika Hewage <randikaudara@gmail.com>
2017-12-25 12:16:53 +0530

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,12 @@
 class ssh::params {
   case $facts['os']['family'] {
     'Debian': {
-      $package_name = 'openssh-server',
-      $service_name = 'ssh',
+      $package_name = 'openssh-server'
+      $service_name = 'ssh'
     }
     'RedHat': {
-      $package_name = 'openssh-server',
-      $service_name = 'sshd',
+      $package_name = 'openssh-server'
+      $service_name = 'sshd'
     }
     default: {
       fail( "${facts['operatingsystem']} is not supported !!!")
