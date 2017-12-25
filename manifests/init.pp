f2dc0dbf3ee83b3789dc5bacb1ddf9afe414594e
Randika Hewage <randikaudara@gmail.com>
2017-12-25 19:43:30 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -46,7 +46,12 @@ class ssh (
   String $package_name = $::ssh::params::package_name,
   String $service_name = $::ssh::params::service_name,
 ) inherits ::ssh::params {
-  class { '::ssh::install': } ->
-  class { '::ssh::service': }
-
+  class { '::ssh::install':
+    #before => Class ['::ssh::service'], 
+  }
+  class { '::ssh::service':
+    require => Class ['::ssh::install'],
+  }
+  
+  #Class ['::ssh::install'] -> Class ['::ssh::service']
 }
